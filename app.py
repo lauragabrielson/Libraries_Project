3f9179c4cf18ea906b14cda509655eec7a8d6471
lauragabrielson <lauramwinge@hotmail.com>
2021-05-01 14:08:18 -0500

    Libraries Route

--- app.py
+++ app.py
@@ -71,27 +71,30 @@ def donut():
     # Return the jsonified result. 
     return jsonify(all_employees)
 
-
 @app.route("/libraries_map")
 def libraries_map():
     ''' Query the database for fighter aircraft and return the results as a JSON. '''
 
     # Open a session, run the query, and then close the session again
     session = Session(engine)
-    results = session.query(table.librarians, table.msl_librarians).all()
+    results = session.query(table.lat, table.lon, table.services_population, table.bookmobiles, table.library_name, table.state).all()
     session.close()
 
     # Create a list of dictionaries, with each dictionary containing one row from the query. 
-    all_aircraft = []
-    for country, iso3, fighteraircraft in results:
+    all_libraries = []
+    for lat, lon, services_population, bookmobiles, library_name, state in results:
         dict = {}
-        dict["country"] = country
-        dict["iso3"] = iso3
-        dict["fighteraircraft"] = fighteraircraft
-        all_aircraft.append(dict)
+        dict["lat"] = lat
+        dict["lon"] = lon
+        dict["services_population"] = services_population
+        dict["bookmobiles"] = bookmobiles
+        dict["library_name"] = library_name
+        dict["state"] = state
+        all_libraries.append(dict)
 
     # Return the jsonified result. 
-    return jsonify(all_aircraft)
+    return jsonify(all_libraries)
+
 
 
 @app.route("/libraries_bar")
@@ -119,59 +122,6 @@ def libraries_bar():
 
     # Return the jsonified result. 
     return jsonify(all_collections)
-
-# @app.route("/population")
-# def QueryPopulation():
-#     ''' Query the database for population numbers and return the results as a JSON. '''
-
-#     # Open a session, run the query, and then close the session again
-#     session = Session(engine)
-#     results = session.query(table.country, table.iso3, table.totalpopulation).all()
-#     session.close 
-
-#     # Create a list of dictionaries, with each dictionary containing one row from the query. 
-#     all_population = []
-#     for country, iso3, totalpopulation in results:
-#         dict = {}
-#         dict["country"] = country
-#         dict["iso3"] = iso3
-#         dict["totalpopulation"] = totalpopulation
-#         all_population.append(dict)
-
-#     # Return the jsonified result. 
-#     return jsonify(all_population)
-
-# @app.route("/test")
-# def TestRoute():
-#     ''' This function returns a simple message, just to guarantee that
-#         the Flask server is working. '''
-
-#     return "This is the test route!"
-
-# @app.route("/dictionary")
-# def DictionaryRoute():
-#     ''' This function returns a jsonified dictionary. Ideally we'd create 
-#         that dictionary from a database query. '''
-
-#     dict = { "Fine Sipping Tequila": 10,
-#              "Beer": 2,
-#              "Red Wine": 8,
-#              "White Wine": 0.25}
-    
-#     return jsonify(dict) # Return the jsonified version of the dictionary
-
-# @app.route("/dict")
-# def DictRoute():
-#     ''' This seems to work in the latest versions of Chrome. But it's WRONG to
-#         return a dictionary (or any Python-specific datatype) without jsonifying
-#         it first! '''        
-
-#     dict = { "one": 1,
-#              "two": 2,
-#              "three": 3}
-    
-#     return dict # WRONG! Don't return a dictionary! Return a JSON instead. 
-
 
 # This statement is required for Flask to do its job. 
 # Think of it as chocolate cake recipe. 
