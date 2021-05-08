6aab342f774f0cbee15e8e3c85d4c3fd7a30bc01
lauragabrielson <lauramwinge@hotmail.com>
2021-05-08 11:59:50 -0500

    Clean files and add comments

--- app.py
+++ app.py
@@ -21,25 +21,20 @@ base.prepare(engine, reflect=True)
 # Choose the table we wish to use
 table = base.classes.libraries
 
-# Instantiate the Flask application. (Chocolate cake recipe.)
-# This statement is required for Flask to do its job. 
+# Instantiate the Flask application
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 # Effectively disables page caching
 
-# Here's where we define the various application routes ...
+# Define the various application routes
 @app.route("/")
 def IndexRoute():
-    ''' This function runs when the browser loads the index route. 
-        Note that the html file must be located in a folder called templates. '''
-
     webpage = render_template("index.html")
     return webpage
 
 
 @app.route("/donut")
 def donut():
-    ''' Query the database and return the results as a JSON. '''
-
+    # Query the database and return the results as a JSON
     # Open a session, run the query, and then close the session again
     session = Session(engine)
     results = session.query(table.mls_librarians, table.librarians, table.employees, table.total_staff, table.library_name, table.state, table.state_name).all()
@@ -63,8 +58,7 @@ def donut():
 
 @app.route("/libraries_map")
 def libraries_map():
-    ''' Query the database and return the results as a JSON. '''
-
+    # Query the database and return the results as a JSON
     # Open a session, run the query, and then close the session again
     session = Session(engine)
     results = session.query(table.lat, table.lon, table.services_population, table.bookmobiles, table.library_name, table.state, table.state_name).all()
@@ -90,8 +84,7 @@ def libraries_map():
 
 @app.route("/libraries_bar")
 def libraries_bar():
-    ''' Query the database and return the results as a JSON. '''
-
+    # Query the database and return the results as a JSON
     # Open a session, run the query, and then close the session again
     session = Session(engine)
     results = session.query(table.print_collection, table.digital_collection, table.audio_collection, table.downloadable_audio, table.physical_video, table.downloadable_video, table.library_name, table.state, table.state_name).all()
@@ -117,8 +110,7 @@ def libraries_bar():
 
 @app.route("/libraries_summary")
 def libraries_summary():
-    ''' Query the database and return the results as a JSON. '''
-
+    # Query the database and return the results as a JSON
     # Open a session, run the query, and then close the session again
     session = Session(engine)
     results = session.query(table.services_population, table.mls_librarians, table.librarians, table.total_staff, table.total_operating_revenue, table.state_name).all()
@@ -140,7 +132,5 @@ def libraries_summary():
     return jsonify(all_state_data)
 
 
-# This statement is required for Flask to do its job. 
-# Think of it as chocolate cake recipe. 
 if __name__ == '__main__':
     app.run(debug=True)
